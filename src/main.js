460e4ef872f2ffee8e3bf30fab0af00d2362079d
quallsma <quallsma@msu.edu>
2017-08-10 09:54:53 -0400

    First Failing Test

---
+++ src/main.js
@@ -0,0 +1,7 @@
+export class Pencil {
+
+}
+
+export class Paper {
+    
+}
--- test/test.js
+++ test/test.js
@@ -1,7 +1,15 @@
 import { expect } from 'chai';
+import sinon from 'sinon';
+import { Pencil, Paper } from '../src/main';
 
-describe("Mocha and chai", () => {
-    it("should be working", () => {
-        expect(true).to.be.true;
+describe('Pencil', () => {
+    it('should write text on paper', () => {
+        var PaperStub = sinon.createStubInstance(Paper);
+        var pencil = new Pencil(PaperStub);
+
+        pencil.write("She sells sea shells");
+        pencil.write(" down by the sea shore");
+
+        expect(pencil.getPaperText()).to.equal('She sells sea shells down by the sea shore');
     });
 });
